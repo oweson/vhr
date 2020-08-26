1ebd63c1c5bf0640246ea1ae3eb93b277869dfe2
chengwl <chengwl@tsintergy.com>
2020-08-26 15:39:44 +0800

    添加上游分支

--- vhr/vhrserver/vhr-web/src/test/java/org/javaboy/vhr/VhrApplicationTests.java
+++ vhr/vhrserver/vhr-web/src/test/java/org/javaboy/vhr/VhrApplicationTests.java
@@ -1,7 +1,10 @@
 package org.javaboy.vhr;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class VhrApplicationTests {
 
+    @Autowired
+    StringEncryptor encryptor;
+    @Autowired
+    private StringEncryptor stringEncryptor;
+
+    @Test
+    public void getPass() {
+        String url = encryptor.encrypt("jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&characterEncoding=utf8");
+        String name = encryptor.encrypt("root");
+        String password = encryptor.encrypt("root");
+        System.out.println(url+"----------------");
+        System.out.println(name+"----------------");
+        System.out.println(password+"----------------");
+        Assert.assertTrue(name.length() > 0);
+        Assert.assertTrue(password.length() > 0);
+    }
+
     @Test
     public void contextLoads() {
+        //加密方法
+        System.out.println(stringEncryptor.encrypt("123456"));
+        System.out.println(stringEncryptor.encrypt("123456"));
+        //解密方法
+//        System.out.println(stringEncryptor.decrypt("uaNBj4ZmzCD83uedRYUXqQ=="));
+//        System.out.println(stringEncryptor.decrypt("oKBQENfbbQiMyPvECAgPGA=="));
     }
 
 }
